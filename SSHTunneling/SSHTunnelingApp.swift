90452a581dfc55485e72dcf8d3b1c63b70cdf379
Julien Guillan <guillan.julien@live.com>
2023-09-21 21:30:17 +0200

    Initial Commit

---
+++ SSHTunneling/SSHTunnelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSHTunnelingApp.swift
+//  SSHTunneling
+//
+//  Created by Julien Guillan on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SSHTunnelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
